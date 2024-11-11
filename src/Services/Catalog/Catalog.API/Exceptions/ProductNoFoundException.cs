0e0e49b8ca947a52df2c2b22350330d28b67f254
ADMIN <ADMIN@DESKTOP-K4GAK5Q>
2024-11-11 22:38:10 +0700

    getProductById

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNoFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductNoFoundException : Exception
+    {
+        public ProductNoFoundException() : base("Product no found!")
+        {
+        }
+    }
+}
